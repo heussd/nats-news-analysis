e9443b0dca9be4c9ba1c65347eaf78aa001c1359
Timm Heuss <heussd@users.noreply.github.com>
2022-07-19 19:38:28 +0100

    Cope with not existing URLs

--- rss-article-url-feeder/RSS.py
+++ rss-article-url-feeder/RSS.py
@@ -4,15 +4,20 @@ from lxml import html
 
 def retrieve_article_links(feedurl):
     article_urls = []
-    page = requests.get(feedurl)
-    tree = html.fromstring(page.content)
-    links = tree.xpath('//item/link')
 
-    for link in links:
-        article_urls.append(link.tail)
+    try:
+        page = requests.get(feedurl)
+        tree = html.fromstring(page.content)
+        links = tree.xpath('//item/link')
+
+        for link in links:
+            article_urls.append(link.tail)
+
+    except:
+        print("Retrieval failed:", feedurl)
 
     return article_urls
 
 
 if __name__ == "__main__":
-    print(retrieve_article_links('https://hnrss.org/newest?count=50'))
+    print(retrieve_article_links('https://hnrss9999.org/newest?count=50'))
