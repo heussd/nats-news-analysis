71d5743a399adcad37c39b847a643fd152d06ed6
Timm Heuss <heussd@users.noreply.github.com>
2022-12-19 21:56:14 +0100

    Introduce opentelemetry

--- keyword-matcher-go/cmd/main.go
+++ keyword-matcher-go/cmd/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/heussd/nats-news-keyword-matcher.go/internal/keywords"
 	queue "github.com/heussd/nats-news-keyword-matcher.go/internal/nats"
 	"github.com/heussd/nats-news-keyword-matcher.go/pkg/fulltextrss"
 	"github.com/nats-io/nats.go"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+	"log"
 	"os"
 	"os/signal"
 	"strings"
@@ -13,12 +21,61 @@ import (
 	"time"
 )
 
+const name = "keyword-matcher-go"
+
+func tracerProvider(url string) (*tracesdk.TracerProvider, error) {
+	// Create the Jaeger exporter
+	//exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint("http://localhost:14268/api/traces")))
+	exp, err := jaeger.New(jaeger.WithAgentEndpoint(jaeger.WithAgentHost("localhost")))
+	if err != nil {
+		return nil, err
+	}
+	tp := tracesdk.NewTracerProvider(
+		// Always be sure to batch in production.
+		tracesdk.WithBatcher(exp),
+		// Record information about this application in a Resource.
+		tracesdk.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(name),
+			attribute.String("environment", "production"),
+			attribute.Int64("ID", 1),
+		)),
+	)
+	return tp, nil
+}
+
 func main() {
+	tp, err := tracerProvider("http://localhost:14268/api/traces")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Register our TracerProvider as the global so any imported
+	// instrumentation in the future will default to using it.
+	otel.SetTracerProvider(tp)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Cleanly shutdown and flush telemetry when the application exits.
+	defer func(ctx context.Context) {
+		// Do not make the application hang when it is shutdown.
+		ctx, cancel = context.WithTimeout(ctx, time.Second*5)
+		defer cancel()
+		if err := tp.Shutdown(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}(ctx)
+	tr := tp.Tracer("component-main")
 
 	queue.WithArticleUrls(func(m *nats.Msg) {
-		var url = string(m.Data)
 		var startTime = time.Now()
+		_, span := tr.Start(context.Background(), "retrieval")
+
+		var url = string(m.Data)
 		var fulltext = fulltextrss.RetrieveFullText(url)
+		span.End()
+
+		_, span = tr.Start(context.Background(), "analysis")
 
 		var text = strings.Join([]string{
 			fulltext.Title,
@@ -28,6 +85,8 @@ func main() {
 
 		var match, matchingText = keywords.Match(text)
 		var elapsedTime = time.Since(startTime)
+		
+		span.End()
 		if match {
 			queue.PushToPocket(url, matchingText)
 			fmt.Printf("✅ %s (analysis took %s)\n", url, elapsedTime)
--- keyword-matcher-go/go.mod
+++ keyword-matcher-go/go.mod
@@ -9,12 +9,20 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/nats-io/nats-server/v2 v2.9.9 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.opentelemetry.io/otel v1.11.2 // indirect
+	go.opentelemetry.io/otel/exporters/jaeger v1.11.2 // indirect
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.11.2 // indirect
+	go.opentelemetry.io/otel/sdk v1.11.2 // indirect
+	go.opentelemetry.io/otel/trace v1.11.2 // indirect
 	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
